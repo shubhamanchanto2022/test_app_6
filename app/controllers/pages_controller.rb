19fbb452a27ae3a5cb2492478c2c3b2e5889e017
shubhamanchanto2022 <shubham.sinha@anchanto.com>
2022-01-12 12:22:48 +0530

    Initial commit and root route

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,4 @@
+class PagesController < ApplicationController
+    def home  # here we only defined method and we have not written anything we will write in views/pages/home.html.erb
+    end
+end
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+Rails.application.routes.draw do
+  root "pages#home" # before '#' that is the name of controller, so for our application it is pages which we can see in controllers
+end
