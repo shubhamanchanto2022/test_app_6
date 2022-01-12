43872be43213409dad6dce8fb66c46dc05ba9313
shubhamanchanto2022 <shubham.sinha@anchanto.com>
2022-01-12 21:29:40 +0530

    new update

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,7 @@
 class PagesController < ApplicationController
     def home  # here we only defined method and we have not written anything we will write in views/pages/home.html.erb
     end
+
+    def about
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  root "pages#home" # before '#' that is the name of controller, so for our application it is pages which we can see in controllers
+  root 'pages#home' # before '#' that is the name of controller, so for our application it is pages which we can see in controllers
+  get 'about', to: 'pages#about'
 end
