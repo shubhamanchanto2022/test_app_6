8a7b29a00d5f84262be7058bbc0efc0f46691a25
shubhamanchanto2022 <shubham.sinha@anchanto.com>
2022-01-13 13:01:22 +0530

    adding scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :articles
   root 'pages#home' # before '#' that is the name of controller, so for our application it is pages which we can see in controllers
   get 'about', to: 'pages#about'
 end
